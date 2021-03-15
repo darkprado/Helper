e7a238d2b72b92eaa425b9cd0a2b448d64e962e1
darkprado <darkprado@yandex.ru>
2021-03-15 14:58:38 +0300

    init

---
+++ src/test/java/my/home/helper/HelperApplicationTests.java
@@ -0,0 +1,13 @@
+package my.home.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
